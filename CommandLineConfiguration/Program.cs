6106b808c513eb2a4fc8f21d0b3a10682378a529
邵汉中 <635260101@qq.com>
2018-05-04 17:24:31 +0800

    命令行配置

---
+++ CommandLineConfiguration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace CommandLineConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "school","哈佛"},
+                { "phone","15893256"}
+            };
+
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(dict)
+                .AddCommandLine(args);
+
+            var configuration = builder.Build();
+            foreach (var item in configuration.AsEnumerable())
+            {
+                Console.WriteLine($"key={item.Key},value={item.Value}");
+            }
+            //Console.WriteLine($"name={configuration["name"]},age={configuration["age"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
